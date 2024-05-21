2ae88ed762e17e7898949c8fd66eb00184dcc93a
anhnq1130 <anhnq@inspirelab.io>
2024-05-21 13:59:25 +0700

    add normalize_data function to utils file

--- utils.py
+++ utils.py
@@ -7,3 +7,30 @@ def load_data(file_path):
 def save_data(data, file_path):
     data.to_csv(file_path, index=False)
     return None
+
+def preprocess_data(data):
+    '''
+    This function will remove any missing values and duplicates from the data.
+    
+    Parameters:
+    data: pandas DataFrame
+    
+    Returns:
+    data: pandas DataFrame
+    '''
+    data = data.dropna()
+    data = data.drop_duplicates()
+    return data
+
+def normalize_data(data):
+    '''
+    This function will normalize the data.
+    
+    Parameters:
+    data: pandas DataFrame
+    
+    Returns:
+    data: pandas DataFrame
+    '''
+    data = (data - data.mean()) / data.std()
+    return data
